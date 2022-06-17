8b83ad338b0e23e15bc2a5f803e669e95412178b
Kenohsi <91662939+Kenohsi@users.noreply.github.com>
2022-06-17 23:22:49 +0200

    wad

--- server.js
+++ server.js
@@ -4,12 +4,37 @@ const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
 
+
+
+//decl. static path
+let staticPath = path.join(__dirname);
+
 //init express.js
-
 const app = express();
-app.get("/", (req, res) => {
 
-res.sendFile(path.join(__dirname, "main.html"));
+//middleware
+app.use(express.static(staticPath));
+
+
+//routes, home Route
+app.get("/", (req, res) => {
+res.sendFile(path.join(staticPath, "main.html"));
+})
+
+//signup route
+app.get('/signup', (req, res)=>{
+    res.sendFile(path.join(staticPath, "signup.html"));
+})
+
+
+///404route
+app.get('/404',(req, res) =>{
+    res.sendFile(path.join(staticPath, "404.html"));
+})
+
+app.use((req, res)=>{
+
+  res.redirect('/404');
 
 })
 
