9c25416d8600141a3c7c4772e386f3e9c6194db7
Kenohsi <91662939+Kenohsi@users.noreply.github.com>
2022-06-20 17:52:50 +0200

    xx

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 const createFooter = () =>{
     let footer = document.querySelector('footer');
     footer.innerHTML = ` <div class ="footer-content">
-    <img src="transparent logog.png" width ="200px" height="60px">
+    <img src="pictures/transparent logog.png" width ="200px" height="60px">
   </div>
   <div class="footer-col-2">
    <p>Follow us on:</p>
@@ -16,15 +16,15 @@ const createFooter = () =>{
    <h5>Contact Us!</h5>
    <i class="fa fa-map-marker" style="width:30px"></i> Vienna, Austria<br>
    <i class="fa fa-phone" style="width:30px"></i> Phone: +4364126165<br>
-   <i class="fa fa-envelope" style="width:30px"> </i> Email: ordergreen@gmail.com<br>
+   <i class="fa fa-envelope" style="width:30px"> </i> Email: <a href="mailto:ordergreen@gmail.com">ordergreen@gmail.com</a><br>
  </div>
 </div>
  <address>
-   Written by <a href="mailto:JaneDoe@gmail.com/">Ordergreen Team</a><br><br>
+   Written by <a href="mailto:ordergreen@gmail.com">Team Ordergreen </a><br><br>
    <p>Download App For Android and IOS mobile phone:</p>
                    <div class="app-logo">
-                       <img src="google_badge.png"width="150px" height="60px">
-                       <img src="apple_badge.jpg" width="150px" height="50px"><br> 
+                       <img  src="pictures/google_badge.png"width="150px" height="60px">
+                       <img src="pictures/apple_badge.jpg" width="150px" height="50px"><br> 
                        <a href="#top">Go to Top</a><br> 
                    </div>                  
 </address>`;
