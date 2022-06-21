2aefa1c4e4219de188080d8f0fbfd25ab0a82bd1
Kenohsi <91662939+Kenohsi@users.noreply.github.com>
2022-06-21 19:05:19 +0200

    5

--- myapp/app.js
+++ myapp/app.js
@@ -1,12 +1,9 @@
-
-
-
-
-
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+
+const fs = require('fs');
 
 const path = require('path');
 let staticPath = path.join(__dirname,"views");
@@ -17,18 +14,31 @@ const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
+var counter=0;
 
 
-const initializePassport = require('./passport-config')
+
+
+const initializePassport = require('./passport-config');
+const { json } = require('body-parser');
+const { count } = require('console');
 initializePassport(
   passport,
   email => users.find(user => user.email === email),
   id => users.find(user => user.id === id)
+  
 )
 
 const users = []
+
+
+
+
+
+
+
 app.engine('html', require('ejs').renderFile);
-// app.set('view-engine', 'html')
+// app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
 app.use(session({
@@ -39,16 +49,23 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
-app.use(express.static(staticPath));
-app.use(express.json());
+app.use(express.static(staticPath))
+app.use(express.json())
 
+
+
+
+//Save User Data in Password
+
+  
 
 app.get('/', checkAuthenticated, (req, res) => {
-  res.render('main.html', { name: req.user.name })
+  res.sendFile(path.join(staticPath, "main.html"));
 })
+// , { name: req.user.name }
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('login.html')
+  res.sendFile(path.join(staticPath, "login.html"));
 })
 
 app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
@@ -58,7 +75,7 @@ app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
 }))
 
 app.get('/signup', checkNotAuthenticated, (req, res) => {
-  res.render('/signup')
+  res.sendFile(path.join(staticPath, "signup.html"));
 })
 
 
@@ -71,7 +88,7 @@ app.get("/", (req, res) => {
 
 //signup route
 app.get('/signup', (req, res) => {
-  res.sendFile(path.join(staticPath, "views/signup.html"));
+  res.sendFile(path.join(staticPath, "signup.html"));
 })
 
 //shop route
@@ -97,22 +114,63 @@ app.get('/404',(req, res) =>{
 app.post('/signup', checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+    
+counter++;
+
     users.push({
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
       password: hashedPassword
     })
+  //  var jsonData = 'USER '+counter+': name: '+ req.body.name+' email: '+req.body.email+' password: '+hashedPassword/n;
+   
+
+   //var jsonSave = JSON.parse( jsonData );
+  //  console.log(jsonData);
+
+
+
+    // //jsonSave= jsonSave.append(jsonData)
+    
+    
+
+    //console.log(jsonParsed.persons[0].name);
+    //var jsonObj = JSON.parse(jsonData);
+//console.log(jsonObj);
+
+
+// stringify JSON Object
+//var jsonContent = JSON.stringify(jsonObj);
+//console.log(jsonContent);
+
+
+
+
+
+
     res.redirect('/login')
   } catch {
     res.redirect('/signup')
-  }
-})
+  } // saveData(users);
 
-// app.delete('/logout', (req, res) => {
-//   req.logOut()
-//   res.redirect('/login')
-// })
+  // console.log(jsonData);
+ // console.log(jsonSave);
+
+})
+// const saveData = (users) =>{
+
+//   // json data
+//   // var jsonData = '{"users":[{"name":"users.name","email":"users.email"}';
+//      //jsonData = JSON.stringify(users)
+  
+  
+//     console.log(users)
+    
+  
+//     }
+    
 
 app.delete('/logout', function(req, res, next) {
   req.logout(function(err) {
@@ -123,13 +181,6 @@ app.delete('/logout', function(req, res, next) {
 
 
 
-
-
-
-
-
-
-
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next()
@@ -137,10 +188,14 @@ function checkAuthenticated(req, res, next) {
 
   res.redirect('/login')
 }
+
+
 app.use((req, res)=>{
     
   res.redirect('/404');
 })
+
+
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return res.redirect('/')
@@ -148,7 +203,13 @@ function checkNotAuthenticated(req, res, next) {
   next()
 }
 
-app.listen(3000)
+
+
+
+app.listen(3000,() => {
+  console.log('listening on port 3000.....');
+  
+  })
 
 
 
@@ -307,10 +368,10 @@ app.listen(3000)
     //   res.redirect('/404');
     // })
 
-//     app.listen(3000,() => {
-//         console.log('listening on port 3000.....');
+    // app.listen(3000,() => {
+    //     console.log('listening on port 3000.....');
         
-//         })
+    //     })
 
 
 
