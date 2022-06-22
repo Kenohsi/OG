a7650b5836718539a65742e034d41930010a2144
ahataha123 <84542936+ahataha123@users.noreply.github.com>
2022-06-22 03:51:58 +0200

    Controlla

--- myapp/views/js/script.js
+++ myapp/views/js/script.js
@@ -6,7 +6,7 @@ const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-// Fetch exchange rates and update the DOM
+
 function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
@@ -14,7 +14,7 @@ function calculate() {
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
     .then(res => res.json())
     .then(data => {
-      // console.log(data);
+      
       const rate = data.rates[currency_two];
 
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
